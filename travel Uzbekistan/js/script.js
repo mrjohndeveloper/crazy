66506494f68106b9861346e88edc50b9cc1a37a4
joxa/coder <125272470+mrjohndeveloper@users.noreply.github.com>
2023-06-20 13:32:27 +0500

    Add files via upload
    take

---
+++ travel Uzbekistan/js/script.js
@@ -0,0 +1,18 @@
+const header = document.querySelector("header");
+
+window.addEventListener ("scroll", function() {
+    header.classList.toggle ("sticky", window.scrollY > 0);
+});
+let menu = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+menu.onclick = () => {
+    menu.classList.toggle("bx-x");
+    menu.classList.toggle("open");
+    navbar.classList.toggle('openning')
+
+};
+window.onscroll = () => {
+    menu.classList.remove("bx-x");
+    menu.classList.remove("open");
+};
